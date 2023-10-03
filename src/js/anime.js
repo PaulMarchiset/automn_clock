a84aa72f650db5acc512ca19876b92d6727f177c
PaulMarchiset <paul.marchiset@edu.univ-fcomte.fr>
2023-10-02 20:01:07 -0400

    dark mode - update

--- src/js/anime.js
+++ src/js/anime.js
@@ -1,1 +1,9 @@
-
+anime({
+    targets: '.leaf_hero .leaf_orange',
+    translateX: -20,
+    rotate: -20,
+    duration: 750,
+    opacity: 1,
+    direction: 'alternate',
+    ease: 'easeInOutSine',
+}) 
