7f690cf31a2a390d2c7f72d558931f1c5ca8ee68
PaulMarchiset <paul.marchiset@edu.univ-fcomte.fr>
2023-10-04 16:02:21 -0400

    timer

--- src/js/app.js
+++ src/js/app.js
@@ -75,59 +75,122 @@ window.setInterval(clockF, 1000);
 
 //    window.setInterval(clockF,1000);
 
-//COUNT DOWN
-let countdown = function () {
-  let s = document.getElementById("down_s").value;
-  let m = document.getElementById("down_m").value;
-  let i;
+// TIMER
 
-  let tick = function () {
-    s = document.getElementById("down_s").value;
-    m = document.getElementById("down_m").value;
-    if (s == 0) {
-      s = 59;
-      m--;
+document.addEventListener("DOMContentLoaded", function() {
+            const minutesInput = document.getElementById("minutes");
+            const secondsInput = document.getElementById("seconds");
+            const startTimerButton = document.getElementById("startTimer");
+            const stopTimerButton = document.getElementById("stopTimer");
+            const timerSVG = document.getElementById("timer");
+            const rectangles = timerSVG.querySelectorAll('rect');
+            
+            let animationTimeout;
+            let animationPaused = false;
+            
+            // Function to update the time remaining text with "00:00" format
+            // Function to update the time remaining text with "00:00" format
+function updateTimeRemainingDisplay(secondsRemaining) {
+    const minutes = Math.floor(secondsRemaining / 60);
+    const seconds = secondsRemaining % 60;
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(seconds).padStart(2, '0');
+    document.getElementById("minutesInput").textContent = formattedMinutes;
+    document.getElementById("secondsInput").textContent = formattedSeconds;
+}
+
+
+            // Function to start or resume the color change animation
+            // Function to start or resume the color change animation
+function startOrResumeColorChangeAnimation() {
+    const minutesInputValue = parseInt(minutesInput.value);
+    const secondsInputValue = parseInt(secondsInput.value);
+    let totalSeconds = minutesInputValue * 60 + secondsInputValue;
+
+    const delayIncrement = totalSeconds * 1000 / rectangles.length;
+
+    if (animationPaused) {
+        rectangles.forEach((rect, index) => {
+            rect.style.animationPlayState = 'running';
+        });
+        animationPaused = false;
     } else {
-      s--;
-    }
-    document.getElementById("down_s").value = s;
-    document.getElementById("down_m").value = m;
+        minutesInput.disabled = true;
+        secondsInput.disabled = true;
+        startTimerButton.disabled = true;
+        stopTimerButton.disabled = false;
 
-    if (s == 0 && m == 0) {
-      clearInterval(i);
-      setTimeout(function () {
-        document.getElementById("down_s").value = 10;
-      }, 2000);
-      document.getElementById("blip").play();
-    }
-  };
+        rectangles.forEach((rect, index) => {
+            rect.style.animation = 'none';
+            rect.getBoundingClientRect();
+        });
 
-  document.getElementById("down_start").addEventListener("click", function () {
-    i = setInterval(tick, 1000);
-  });
-  document.getElementById("down_stop").addEventListener("click", function () {
-    clearInterval(i);
-  });
-};
-countdown();
+        rectangles.forEach((rect, index) => {
+            rect.style.animation = `colorChangeAnimation ${totalSeconds}s linear forwards ${index * delayIncrement}ms`;
+        });
+
+        animationTimeout = setInterval(function() {
+            if (totalSeconds > 0) {
+                totalSeconds--;
+                document.getElementById("seconds").value = document.getElementById("seconds").value - 1;
+                document.getElementById("minutes").value = document.getElementById("minutes").value - 1;
+                updateTimeRemainingDisplay(totalSeconds);
+            } 
+            else {
+                clearInterval(animationTimeout);
+                stopColorChangeAnimation();
+            }
+        }, 1000);
+    }
+
+    // Update the time remaining display initially
+    updateTimeRemainingDisplay(totalSeconds);
+}
+
+            
+            // Function to stop the color change animation
+            function stopColorChangeAnimation() {
+                minutesInput.disabled = false;
+                secondsInput.disabled = false;
+                startTimerButton.disabled = false;
+                stopTimerButton.disabled = false;                
+                rectangles.forEach((rect) => {
+                    rect.style.animationPlayState = 'paused';
+                });
+                
+                clearTimeout(animationTimeout);
+                animationPaused = true;
+
+                // Clear the time remaining display
+                document.getElementById("timeRemaining").textContent = "00:00";
+            }
+            
+            startTimerButton.addEventListener("click", startOrResumeColorChangeAnimation);
+            stopTimerButton.addEventListener("click", stopColorChangeAnimation);
+        }); 
 
 //STOP WATCH
 let watch = function () {
   let s = document.getElementById("watch_s").value;
   let m = document.getElementById("watch_m").value;
+  let h = document.getElementById("watch_h").value;
   let i;
 
   let tick = function () {
     s = document.getElementById("watch_s").value;
     m = document.getElementById("watch_m").value;
+    h = document.getElementById("watch_h").value;
     if (s == 59) {
       s = 0;
       m++;
+    } else if (m == 59) {
+        m = 0;
+        h++;
     } else {
-      s++;
-    }
+        s++; }
     document.getElementById("watch_s").value = s;
     document.getElementById("watch_m").value = m;
+    document.getElementById("watch_h").value = h;
   };
 
   document.getElementById("watch_start").addEventListener("click", function () {
@@ -139,6 +202,7 @@ let watch = function () {
   document.getElementById("watch_reset").addEventListener("click", function () {
     document.getElementById("watch_s").value = 0;
     document.getElementById("watch_m").value = 0;
+    document.getElementById("watch_h").value = 0;
     //clearInterval(i);
   });
 };
@@ -207,6 +271,7 @@ watch();
       
         c.style.display = "flex";
         buttonClock.classList.add("active");
+        c.classList.add("show");
         d.style.display = "none";
         w.style.display = "none";
       
@@ -241,4 +306,3 @@ watch();
       }
       
       ui();
-      
